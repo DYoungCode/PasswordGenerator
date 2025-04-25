4690fc86cea990e4cdd680e11a47460333efeede
Don Young <don.f.young@gmail.com>
2025-04-25 14:11:24 -0400

    This is my attempt at a password generator
    It uses a for loop to call a function to get input from the user. The
    for loop calls a list containing [lower, upper, digits, special] so the
    question can be asked 4 times, 1 for each element in the list
    
    The input is added to a dictionary:
    key = lower, upper, digits, special
    value = number of characters selected by user
    
    For loop cycles through keys, nested for loop generates random value based
    on number of characters selected by user. Random characters are added to a
    loop. Loops is then randomized and password is displayed to user.

--- random.py
+++ random.py
@@ -1,17 +1,20 @@
-
-#user_input = input("Password length(integer):")
-
-#try:
-#    number = int(user_input)
-#    print("Success!")
-#except:
-#    print("That's not an integer")
+import random
+import string
 
 count = 0
 total = 0
+char_dict = {}      #key: type of char (lower, upper, digit, special), value: how many user selected
+random_list = []    #store randomly generated characters in list
+
+### Todo ###
+# optional - add a "just surprise me option"
+
+print("This program will generate a random password based on your inputs.")
 
 char_types = ["lowercase characters", "uppercase characters", "digits", "special characters"]
-char_dict = {}
+
+# this function takes in 3 prompts (2 strings, 1 list) to ask the user
+# a question and variable created to store users response
 def get_user_input(prompt1, prompt2, prompt3, data_type=int):
     prompt = prompt1 + " " + prompt2 + " " + prompt3
     while True:
@@ -21,6 +24,7 @@ def get_user_input(prompt1, prompt2, prompt3, data_type=int):
         except ValueError: 
             print("Invalid input.")
 
+# call get_user_input func, return value to a dictionary and capture total characters in password
 for x in char_types:
     return_value = get_user_input("How many", x, "do you want in your password?")
     char_dict[x] = return_value
@@ -29,8 +33,25 @@ for x in char_types:
 print(char_dict)
 print("The total number of characters is", total)
 
-#lcase = get_user_input("How many lowercase characters do you want in your password?")
-#ucase = get_user_input("How many uppercase characters do you want in your password?")
-#digit = get_user_input("How many digits do you want in your password?")
-#special = get_user_input("How many special characters do you want in your password?")
+for key in char_dict:
+    value = char_dict[key]
+    for x in range(value):
+        if key == 'lowercase characters':
+            random_lower = random.choice(string.ascii_lowercase)
+            random_list.append(random_lower)
+        elif key == 'uppercase characters':
+            random_upper = random.choice(string.ascii_uppercase)
+            random_list.append(random_upper)
+        elif key == 'digits':
+            random_digits = random.choice(string.digits)
+            random_list.append(random_digits)
+        elif key == 'special characters':
+            random_special = random.choice(string.punctuation)
+            random_list.append(random_special)
+        #print(x)
+    #print(f"Key: {key} Value: {value}")   #Debugging
 
+random.shuffle(random_list)
+combined_string = ''.join(random_list)
+print("\nYour randomly generated password is:", combined_string)
+# type: ignore
